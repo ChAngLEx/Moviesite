4f2aeb25cd127d867752f530a795b6335834cb33
Chang <kicsy96@126.com>
2020-04-12 04:13:43 -0400

    Signed-off-by: Chang <kicsy96@126.com>

--- group/views.py
+++ group/views.py
@@ -7,13 +7,16 @@ from django.http import HttpResponse, JsonResponse
 from django_redis import get_redis_connection
 from django.core.cache import cache
 from user.models import User
-from .models import Group, UserGroup, Event, Vote, VoteRecord
+from .models import Group, UserGroup, Event, Vote, VoteRecord, MovieList
 from movie.models import Movie
+from django.db.models import Q
+import collections
 
 class IndexView(View):
     def get(self, request):
         '''显示首页'''
-        groups = Group.objects.all()
+        user = request.user.id
+        groups = Group.objects.filter(group_user_id=user)
 
         context = {'groups': groups}
 
@@ -25,7 +28,7 @@ class GroupCreateView(View):
         return render(request, 'create_group.html')
 
     def post(self, request):
-        group_user = request.POST.get('user_name')
+
         group_name = request.POST.get('group_name')
         user = request.user
 
@@ -53,7 +56,7 @@ class GroupCreateView(View):
 
 
         group_user_id = '%d'%user.id
-        group = Group.objects.create(group_user_id=group_user_id, group_name=group_name, group_user=group_user)
+        group = Group.objects.create(group_user_id=group_user_id, group_name=group_name)
         group.save()
 
         return redirect(reverse('group:index'))
@@ -69,7 +72,8 @@ class GroupJoinView(View):
             return render(request, 'create_group.html', {'errmsg': 'Required sign in'})
 
         #receive info
-        user = request.POST.get('username')
+
+        user = request.user.id
         group = request.POST.get('group_name')
 
         #try:
@@ -83,7 +87,12 @@ class GroupJoinView(View):
             # 用户名已存在
             #return JsonResponse({'res':2, 'errmsg':'user already joined in'})
 
-        Group.objects.filter(group_name=group).update(group_user=user)
+        group_id = Group.objects.get(group_name=group).id
+
+        #user_group_id ='%d'%user1.id
+        a = UserGroup.objects.create(group_id=group_id, user_id=user)
+        a.save()
+        #Group.objects.filter(group_name=group).update(group_user=user)
 
         return redirect(reverse('group:index'))
 
@@ -111,8 +120,11 @@ class GroupUnsubscribeView(View):
         # if username in group:
         # 用户名已存在
         # return JsonResponse({'res':2, 'errmsg':'user already joined in'})
+        #group_user_id = Group.objects.filter(group_name=group).get(group)
+        user_id = User.objects.get(username=user).id
+        group_id = Group.objects.get(group_name=group).id
 
-        Group.objects.filter(group_name=group).update(group_user=None)
+        UserGroup.objects.filter(Q(group_id=group_id) & Q(user_id=user_id)).delete()
         return redirect(reverse('group:index'))
 
 class EventCreateview(View):
@@ -123,20 +135,21 @@ class EventCreateview(View):
 
     def post(self, request):
         event_name = request.POST.get('event_name')
-        event_movie = request.POST.get('movie_name')
-        event_group = request.POST.get('group_name')
+        event_movie = request.POST.get('event_movie')
+        event_group = request.POST.get('event_group')
 
         #movie = request.movie
         #group = request.group
+        #print("*********************************************************************************************")
 
-
-        if not all([event_name, event_movie, event_group]):
+        #if not all([event_name, event_movie, event_group]):
             # lack data
-            return render(request, 'create_group.html', {'errmsg': 'Requiring more information'})
+            #return render(request, 'create_group.html', {'errmsg': 'Requiring more information'})
 
         # if user.user_type == 0:
         # return render(request, 'create_group.html', {'errmsg': 'illegal user'})
-
+        #print("_" * 122)
+        #print(event_movie)
         try:
             event = Event.objects.get(event_name=event_name)
 
@@ -152,9 +165,11 @@ class EventCreateview(View):
         #event.event_name = event_name
         #event.event_movie = event_movie
         #event.event_group = event_group
-
+        event_movie_id = MovieList.objects.get(movie_name=event_movie).id
+        event_group_id = Group.objects.get(group_name=event_group).id
+        print(event_movie_id, event_group_id)
         #group_user_id = '%d'%user.id
-        event = Event.objects.create(event_name=event_name, event_movie=event_movie, event_group=event_group)
+        event = Event.objects.create(event_name=event_name, event_movie_id=event_movie_id, event_group_id=event_group_id)
         event.save()
 
         return redirect(reverse('group:event'))
@@ -162,23 +177,13 @@ class EventCreateview(View):
 class EventView(View):
     def get(self, request):
         '''显示首页'''
-        # 尝试从缓存中获取数据
-        context = cache.get('event_page_data')
-        
-        if context is None:
-            print('Setting Cache')
-            # 缓存中没有数据
+        #user_id = request.user.id
+        #group_id = UserGroup.objects.all()
 
-            events = Event.objects.all()
-            groups = Group.objects.all()
+        #events = Event.objects.filter(event_group_id=group_id)
 
-            context = {'events': events,
-                       'groups': groups,
-                       }
-            # 设置缓存
-            # key  value timeout
-            cache.set('index_page_data', context, 3600)
-    
+        events = Event.objects.all()
+        context = {'events': events}
 
         return render(request, 'event.html', context)
 
@@ -220,6 +225,8 @@ class VoteCreateview(View):
         #vote.open_time = open_time
         #vote.close_time = close_time
         #vote.vote_event = vote_event
+        vote_movie = MovieList.objects.get(movie_name=vote_movie)
+        vote_event = Event.objects.get(event_name=vote_event)
         vote = Vote.objects.create(vote_name=vote_name, vote_movie=vote_movie, close_time=close_time, vote_event=vote_event)
         vote.save()
 
@@ -245,22 +252,36 @@ class VoteDetailView(View):
         if not user.is_authenticated:
             return JsonResponse({'res': 0, 'errmsg': 'User is not signed in!'})
 
+        #user_id = request.user.id
+        #group_id = Group.objects.get(group_user_id=user_id).id
+        #event_id = Event.objects.get(event_group_id=group_id).id
+        #vote_id = Vote.objects.get(vote_event_id=event_id)
         vote_name = request.POST.get('vote_name')
+        vote_id = Vote.objects.get(vote_name=vote_name).id
         vote_record = request.POST.get('vote_record')
 
 
-        if not all([vote_name, vote_record]):
+        #if not all([vote_name, vote_record]):
             # lack data
-            return render(request, 'vote_detail.html', {'errmsg': 'Requiring more information'})
+            #return render(request, 'vote_detail.html', {'errmsg': 'Requiring more information'})
 
         # if user.user_type == 0:
         # return render(request, 'create_group.html', {'errmsg': 'illegal user'})
 
-        vote_record = VoteRecord()
-        vote_record.vote = vote_name
-        vote_record.vote_record = vote_record
+        #vote_record = VoteRecord()
+        #vote_record.vote = vote_name
+        #vote_record.vote_record = vote_record
+        #vote_id = 1
+        #vote_record = 1
+        if vote_record == 1:
+            vote_result = 1
+        else:
+            vote_result = 0
+        vote_record = VoteRecord.objects.create(vote_record=vote_result, vote_id=vote_id)
+        vote_record.save()
 
-        return redirect(reverse('group:vote_detail'))
+        return redirect(reverse('group:vote_record'))
+
 
 
 class VoteRecordView(View):
@@ -269,14 +290,14 @@ class VoteRecordView(View):
 
         records = VoteRecord.objects.all()
 
-        context = {'records': records}
+        context = {"Yes": 0, "No": 0}
+        for record in records:
+            if record.vote_record:
+                context["Yes"] += 1
+            else:
+                context["No"] += 1
 
         return render(request, 'vote_record.html', context)   
 
 
-
-
-
-
-# Create your views here.
 
